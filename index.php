1927f437806e03c1f5eb983a6a013826ab4cf463
Oleg <mr.dvorezky@gmail.com>
2017-03-21 16:08:47 +0300

    create project

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include "php/mail.php";?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+ <title>Простая форма PHP+математическая капча</title>
+</head>
+<body>
+<?php if(!empty($_SESSION['mes'])): ?>
+   <script>alert('<?php echo $_SESSION['mes'] ?>')</script>
+   <?php $_SESSION['mes'] = null; ?>
+<?php endif; ?>
+<form method="post" action="">
+    <table>
+        <tr>
+            <td>Текст: </td><td><input type="text" name="text" required/>       </td>
+        </tr>
+        <tr>
+            <td>Выражение: </td><td><input type="text" name="res" required/></td>
+        </tr>
+        <tr>
+            <td><?php echo $a. ' + ' .$b. ' = ';?></td>
+            <td><input type="text" name="res" /></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" name="submit" value="отправить" /></td>
+        </tr> 
+    </table>
+</form>
+</body> 
+</html> 
---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+if($_POST['submit']){
+	if($_POST['res'] == $_SESSION['res']){
+		$to = 'mail@mail.ru';
+		$subject = 'Письмо с сайта';
+		$body = $_POST['text'];
+		$headers = 'Content-type:text/plain; Charset=windows-1251';
+		
+		if(mail($to, $subject, $body, $headers)){
+			$_SESSION['mes'] = 'Письмо отправлено!';
+			header("Location: http://localhost:3000");
+			exit();
+		}else{
+			$_SESSION['mes'] = '<p>Ошибка!</p>';
+			header("Location: http://localhost:3000");
+			exit();
+		}
+	}else{
+		$_SESSION['mes'] = 'Дан неверный ответ!';
+		header("Location: http://localhost:3000");
+		exit();
+	}
+}
+
+$a = rand(1,10);
+$b = rand(1,10);
+$_SESSION['res'] = $a + $b;
+?>
+
